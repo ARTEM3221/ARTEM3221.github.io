aa7f5baa7e32e5e23c01ec111d36b8372d6ed8fc
ARTEM3221 <goroholinsyartem@gmail.com>
2022-05-19 17:58:08 +0300

    1

---
+++ js/mainvue.js
@@ -0,0 +1,22 @@
+var app = new Vue ({
+	el: '#article',
+	data:{
+		products:[
+		{id:1,title:"Vidnaya", short_text:'A typical summer variety, which is a medium-sized tree with a narrow pyramidal crown with curved, brown, bare and rounded shoots.', image:'1.jpg', desc: "33"},
+		{id:2,title:"Bryansk beauty", short_text:'A typical late summer variety, which is a medium-sized tree, weakly growing, with a crown in the form of a rarefied ball with compact, brownish-brown, cranked, bare and rounded shoots, the ends of which are directed upwards.', image:'2.jpg', desc: "Full desc"},
+		{id:3,title:"Same age", short_text:'A typical late summer variety, which is a medium-sized tree with a crown in the form of a rounded compact pyramid of medium density.', image:'3.jpg', desc: "Full desc"},
+		{id:4,title:"Banana", short_text:'A typical summer variety, which is a medium-sized tree with a drooping, medium-thickened crown and curved, brown, bare and rounded shoots.', image:'4.jpg', desc: "Full desc"},
+		{id:5,title:"Skorospelka from Michurinsk", short_text:'A typical early summer variety, which is a medium-sized tree with a crown in the form of a rounded pyramid of medium density.', image:'5.jpg', desc: "Full desc"}]
+	},
+
+	mounted: function() {
+		console.log(window.localStorage.getItem('prod'));
+	},
+
+	methods:{
+		addItem:function(id){
+			window.localStorage.setItem('prod', id);
+		}
+
+	}
+});
